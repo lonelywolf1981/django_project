1d89d82efce4f6b8d8340e1f7130151151526fee
lonelywolf1981 <andrey.lw@gmail.com>
2020-09-13 14:54:45 +0600

    Form for post create auto generate slug with translit Make Mixin for post and tag create
    Signed-off-by: lonelywolf1981 <andrey.lw@gmail.com>

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.views.generic import View
-from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .forms import *
+from .utils import *
+from .models import *
 
 
-
-# Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
@@ -27,14 +24,12 @@ class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
 
-    def post(self, request):
-        bound_form = TagForm(request.POST)
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+class TagCreate(ObjectCreateMixin, View):
+    form_model = TagForm
+    template = 'blog/tag_create.html'
+
+
+class PostCreate(ObjectCreateMixin, View):
+    form_model = PostForm
+    template = 'blog/post_create.html'
